07fc706fc10cdb4161f861033c7277c4f1bb371b
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-01-06 18:36:20 +0100

    file updated

--- srcs/general/init_window.c
+++ srcs/general/init_window.c
@@ -6,19 +6,11 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 14:05:42 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/01/02 16:10:12 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/01/06 18:07:30 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static void	init_window_size(t_window *window)
-{
-	// window->win_width = 1920;
-	// window->win_height = 1080;
-	window->win_width = 1024;
-	window->win_height = 512;
-}
 
 /*
 	init t_window datas
@@ -26,11 +18,6 @@ static void	init_window_size(t_window *window)
 void	init_window(t_window *window)
 {
 	window->mlx_id = mlx_init();
-	init_window_size(window);
-	window->win_id = mlx_new_window(
-		window->mlx_id,
-		window->win_width,
-		window->win_height,
-		"Half Life 3.2"
-	);
+	window->win_id = mlx_new_window(window->mlx_id, WIN_WIDTH, WIN_HEIGTH,
+		"Half Life 3.2");
 }
