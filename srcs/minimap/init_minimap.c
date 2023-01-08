c5d72bb54326386140d34b997650c6489728ff5d
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-01-08 15:09:46 +0100

    rename functions

--- srcs/minimap/init_minimap.c
+++ srcs/minimap/init_minimap.c
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 15:05:48 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/01/07 16:14:44 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/01/08 14:44:45 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ enum e_minimap
 
 /*
 	Run through the map and display *img where its letter is placed
+	Get player coordinate and direction
+	Try to read less times as possible
 */
 void	init_minimap(t_global *global)
 {
@@ -33,14 +35,41 @@ void	init_minimap(t_global *global)
 		while (global->map_datas.map[i][++j])
 		{
 			if (global->map_datas.map[i][j] == MINI_WALL)
-				my_mlx_put_sqare(global, j * MINI_WIDTH, i * MINI_HEIGHT, BLACK);
+				my_mlx_put_square(global, j * MINI_WIDTH, i * MINI_HEIGHT, BLACK);
 			else if (global->map_datas.map[i][j] == 'N'
 				|| global->map_datas.map[i][j] == 'S'
 				|| global->map_datas.map[i][j] == 'E'
 				|| global->map_datas.map[i][j] == 'W')
-				my_mlx_pixel_put(&global->minimap, i, j, COLOR);
+				my_mlx_put_square(global, j * MINI_WIDTH, i * MINI_HEIGHT, COLOR);
+
 			else
-				my_mlx_put_sqare(global, j * MINI_WIDTH, i * MINI_HEIGHT, WHITE);
+				my_mlx_put_square(global, j * MINI_WIDTH, i * MINI_HEIGHT, WHITE);
 		}
 	}
 }
+// void	init_minimap(t_global *global)
+// {
+// 	int		i;
+// 	int		j;
+
+// 	i = -1;
+// 	while (global->map_datas.map[++i])
+// 	{
+// 		j = -1;
+// 		while (global->map_datas.map[i][++j])
+// 		{
+// 			if (global->map_datas.map[i][j] == MINI_WALL)
+// 				my_mlx_put_sqare(global, j * MINI_WIDTH, i * MINI_HEIGHT, BLACK);
+// 			else if (global->map_datas.map[i][j] == 'N'
+// 				|| global->map_datas.map[i][j] == 'S'
+// 				|| global->map_datas.map[i][j] == 'E'
+// 				|| global->map_datas.map[i][j] == 'W')
+// 				mlx_pixel_put(global->window.mlx_id,
+// 					global->window.win_id,
+// 					j * MINI_WIDTH,
+// 					i * MINI_HEIGHT, COLOR);
+// 			else
+// 				my_mlx_put_sqare(global, j * MINI_WIDTH, i * MINI_HEIGHT, WHITE);
+// 		}
+// 	}
+// }
