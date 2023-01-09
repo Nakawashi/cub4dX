438850e72d2e2102dc203afb134398bd475715a7
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-01-09 18:37:44 +0100

    small update

--- srcs/hooks/events.c
+++ srcs/hooks/events.c
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 13:43:13 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/01/09 13:16:55 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/01/09 18:21:01 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	move_north(t_player *player)
 {
 	player->y  -= 1; // on check pas ici les limites de la map mais si on est contre un mur on va pas
-	printf("player horizontal : %d\n", player->x);
-	printf("player vertical : %d\n", player->y);
+	printf("player horizontal : [%d]\n", player->x);
+	printf("player vertical : [%d]\n", player->y);
 }
 
 int	key_hook(int keycode, t_global *global)
