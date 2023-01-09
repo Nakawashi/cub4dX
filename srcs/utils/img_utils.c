a7e25d7586049c7e264ab49184b9b6bd58cb33c6
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-01-09 15:41:52 +0100

    utils directory added

---
+++ srcs/utils/img_utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   img_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 20:05:57 by lgenevey          #+#    #+#             */
+/*   Updated: 2023/01/09 13:10:37 by lgenevey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+/*
+	https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html
+
+	description : remplace pixel_put qui affiche un pixel et refresh l'affichage
+	ici on cree un buffer qui va stocker les pixels et les afficher en meme tps
+	x and y sont les coordonnees de la fenetre.
+	0, 0 est tout en haut a gauche.
+*/
+void	his_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	init_img_struct(t_img *img)
+{
+	img->img = NULL;
+	img->addr = NULL;
+	img->texture_path = NULL;
+	img->img_width = 0;
+	img->img_height = 0;
+	img->bits_per_pixel = 0;
+	img->line_length = 0;
+	img->endian = 0;
+}
+
+void	create_image(t_img *img, t_window *mlx_id, int width, int height)
+{
+	init_img_struct(img);
+	img->img = mlx_new_image(mlx_id, width, height);
+	img->addr = mlx_get_data_addr(
+		img->img,
+		&img->bits_per_pixel,
+		&img->line_length,
+		&img->endian);
+}
