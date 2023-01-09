7fe2faaf07771c627dab793a7ec43d09afac4b57
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-01-09 19:05:21 +0100

    print wasd pressed for test

--- srcs/hooks/events.c
+++ srcs/hooks/events.c
@@ -6,15 +6,34 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 13:43:13 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/01/09 18:21:01 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/01/09 19:04:53 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	move_north(t_player *player)
+static void	move_north(t_player *player)
 {
-	player->y  -= 1; // on check pas ici les limites de la map mais si on est contre un mur on va pas
+	player->y  += 1; // on check pas ici les limites de la map mais si on est contre un mur on va pas
+	printf("player horizontal : [%d]\n", player->x);
+	printf("player vertical : [%d]\n", player->y);
+}
+
+static void	move_south(t_player *player)
+{
+	player->y -= 1;
+	printf("player horizontal : [%d]\n", player->x);
+	printf("player vertical : [%d]\n", player->y);
+}
+static void	move_east(t_player *player)
+{
+	player->x += 1;
+	printf("player horizontal : [%d]\n", player->x);
+	printf("player vertical : [%d]\n", player->y);
+}
+static void	move_west(t_player *player)
+{
+	player->x -= 1;
 	printf("player horizontal : [%d]\n", player->x);
 	printf("player vertical : [%d]\n", player->y);
 }
@@ -29,10 +48,19 @@ int	key_hook(int keycode, t_global *global)
 		printf("you pressed w\n");
 	}
 	if (keycode == KEY_A)
+	{
+		move_west(&global->player);
 		printf("you pressed a\n");
+	}
 	if (keycode == KEY_S)
+	{
+		move_south(&global->player);
 		printf("you pressed s\n");
+	}
 	if (keycode == KEY_D)
+	{
+		move_east(&global->player);
 		printf("you pressed d\n");
+	}
 	return (0);
 }
