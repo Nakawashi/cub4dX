b2089bf365415de03defa09ba2a8fa88f7b85022
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2023-02-14 11:55:09 +0100

    Norm hook folder OK

--- srcs/hooks/events.c
+++ srcs/hooks/events.c
@@ -6,84 +6,30 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 13:43:13 by lgenevey          #+#    #+#             */
-/*   Updated: 2023/02/14 11:08:32 by lgenevey         ###   ########.fr       */
+/*   Updated: 2023/02/14 11:54:24 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	move_forward(t_global *global)
+static void	show_map(t_global *global)
 {
-	// si ou je vais ya un mur ou pas, ne pas avancer si oui
-	t_vector2_f futur;
-
-	futur = global->player.pos;
-	// printf("\nactual X: %f\n", global->player.pos.x);
-	// printf("actual Y: %f\n\n", global->player.pos.y);
-
-	futur.x += cos(global->player.initial_angle) * global->player.speed;
-	futur.y += sin(global->player.initial_angle) * global->player.speed;
-	// printf("futur X: %f\n", futur.x);
-	// printf("futur Y: %f\n\n", futur.y);
-
-	// printf("actual coordonnee X: %d\n", pixels_to_coordinate(global->player.pos.x));
-	// printf("actual coordonnee Y: %d\n", pixels_to_coordinate(global->player.pos.y));
-	// printf("futur coordonnee X: %d\n", pixels_to_coordinate(futur.x));
-	// printf("futur coordonnee Y: %d\n\n", pixels_to_coordinate(futur.y));
-
-
-	// printf("futur X : %d\n", global->map_datas.map[pixels_to_coordinate(futur.y)][pixels_to_coordinate(futur.x)]);
-	if (global->map_datas.map[pixels_to_coordinate(futur.y)][pixels_to_coordinate(futur.x)] == '0')
-	{
-		global->player.pos.x += cos(global->player.initial_angle) * global->player.speed;
-		global->player.pos.y += sin(global->player.initial_angle) * global->player.speed;
-	}
-
-}
-
-static void	move_backward(t_global *global)
-{
-	t_vector2_f futur;
-
-	futur = global->player.pos;
-	futur.x -= cos(global->player.initial_angle) * global->player.speed;
-	futur.y -= sin(global->player.initial_angle) * global->player.speed;
-	if (global->map_datas.map[pixels_to_coordinate(futur.y)][pixels_to_coordinate(futur.x)] == '0')
-	{
-		global->player.pos.x -= cos(global->player.initial_angle) * global->player.speed;
-		global->player.pos.y -= sin(global->player.initial_angle) * global->player.speed;
-	}
-}
-static void	move_right(t_global *global)
-{
-	t_vector2_f futur;
-
-	futur = global->player.pos;
-	futur.x += cos(global->player.initial_angle + deg_to_rad(90)) * global->player.speed;
-	futur.y += sin(global->player.initial_angle + deg_to_rad(90)) * global->player.speed;
-	if (global->map_datas.map[pixels_to_coordinate(futur.y)][pixels_to_coordinate(futur.x)] == '0')
-	{
-		global->player.pos.x += cos(global->player.initial_angle + deg_to_rad(90)) * global->player.speed;
-		global->player.pos.y += sin(global->player.initial_angle + deg_to_rad(90)) * global->player.speed;
-	}
-}
-static void	move_left(t_global *global)
-{
-	t_vector2_f futur;
-
-	futur = global->player.pos;
-	futur.x += cos(global->player.initial_angle + deg_to_rad(-90)) * global->player.speed;
-	futur.y += sin(global->player.initial_angle + deg_to_rad(-90)) * global->player.speed;
-	if (global->map_datas.map[pixels_to_coordinate(futur.y)][pixels_to_coordinate(futur.x)] == '0')
-	{
-		global->player.pos.y += sin(global->player.initial_angle + deg_to_rad(-90)) * global->player.speed;
-		global->player.pos.x += cos(global->player.initial_angle + deg_to_rad(-90)) * global->player.speed;
-	}
+	mlx_put_image_to_window(
+		global->window.mlx_id,
+		global->window.win_id,
+		global->minimap.img, 0, 0);
+	mlx_put_image_to_window(
+		global->window.mlx_id,
+		global->window.win_id,
+		global->player.img.img,
+		global->player.pos.x - 1.0,
+		global->player.pos.y - 1.0);
 }
 
 int	key_hook(int keycode, t_global *global)
 {
-	static int map = 0;
+	static int	map = 0;
+
 	if (keycode == KEY_ESC)
 		clean(global);
 	if (keycode == KEY_W)
@@ -98,26 +44,14 @@ int	key_hook(int keycode, t_global *global)
 		global->player.initial_angle -= 0.08;
 	if (keycode == ARROW_RIGHT)
 		global->player.initial_angle += 0.08;
-	if (keycode == 46) // KEY_M
+	if (keycode == 46)
 		map = !map;
-
-	draw_minimap(global); // clear minimap
+	draw_minimap(global);
 	draw_rainbow(global);
-	mlx_put_image_to_window(global->window.mlx_id, global->window.win_id, global->render_img.img, 0, 0);
-
+	mlx_put_image_to_window(global->window.mlx_id,
+		global->window.win_id, global->render_img.img, 0, 0);
 	if (map)
-	{
-		mlx_put_image_to_window(
-			global->window.mlx_id,
-			global->window.win_id,
-			global->minimap.img, 0, 0);
-		mlx_put_image_to_window(
-			global->window.mlx_id,
-			global->window.win_id,
-			global->player.img.img,
-			global->player.pos.x - 1.0,
-			global->player.pos.y - 1.0);
-	}
+		show_map(global);
 	return (0);
 }
 
---
+++ srcs/hooks/events_utils.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 11:50:56 by lgenevey          #+#    #+#             */
+/*   Updated: 2023/02/14 11:52:54 by lgenevey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	move_forward(t_global *g)
+{
+	t_vector2_f	futur;
+
+	futur = g->player.pos;
+	futur.x += cos(g->player.initial_angle) * g->player.speed;
+	futur.y += sin(g->player.initial_angle) * g->player.speed;
+	if (g->map_datas.map[pxl_to_coord(futur.y)][pxl_to_coord(futur.x)] == '0')
+	{
+		g->player.pos.x += cos(g->player.initial_angle) * g->player.speed;
+		g->player.pos.y += sin(g->player.initial_angle) * g->player.speed;
+	}
+}
+
+void	move_backward(t_global *g)
+{
+	t_vector2_f	futur;
+
+	futur = g->player.pos;
+	futur.x -= cos(g->player.initial_angle) * g->player.speed;
+	futur.y -= sin(g->player.initial_angle) * g->player.speed;
+	if (g->map_datas.map[pxl_to_coord(futur.y)][pxl_to_coord(futur.x)] == '0')
+	{
+		g->player.pos.x -= cos(g->player.initial_angle) * g->player.speed;
+		g->player.pos.y -= sin(g->player.initial_angle) * g->player.speed;
+	}
+}
+
+void	move_right(t_global *g)
+{
+	t_vector2_f	futur;
+
+	futur = g->player.pos;
+	futur.x += cos(g->player.initial_angle + deg_to_rad(90)) * g->player.speed;
+	futur.y += sin(g->player.initial_angle + deg_to_rad(90)) * g->player.speed;
+	if (g->map_datas.map[pxl_to_coord(futur.y)][pxl_to_coord(futur.x)] == '0')
+	{
+		g->player.pos.x
+			+= cos(g->player.initial_angle + deg_to_rad(90)) * g->player.speed;
+		g->player.pos.y
+			+= sin(g->player.initial_angle + deg_to_rad(90)) * g->player.speed;
+	}
+}
+
+void	move_left(t_global *g)
+{
+	t_vector2_f	futur;
+
+	futur = g->player.pos;
+	futur.x += cos(g->player.initial_angle + deg_to_rad(-90)) * g->player.speed;
+	futur.y += sin(g->player.initial_angle + deg_to_rad(-90)) * g->player.speed;
+	if (g->map_datas.map[pxl_to_coord(futur.y)][pxl_to_coord(futur.x)] == '0')
+	{
+		g->player.pos.y
+			+= sin(g->player.initial_angle + deg_to_rad(-90)) * g->player.speed;
+		g->player.pos.x
+			+= cos(g->player.initial_angle + deg_to_rad(-90)) * g->player.speed;
+	}
+}
+
+// static void	move_forward(t_global *g)
+// {
+// 	t_vector2_f futur;
+
+// 	futur = g->player.pos;
+// 	printf("\nactual X: %f\n", g->player.pos.x);
+// 	printf("actual Y: %f\n\n", g->player.pos.y);
+
+// 	futur.x += cos(g->player.initial_angle) * g->player.speed;
+// 	futur.y += sin(g->player.initial_angle) * g->player.speed;
+// 	printf("futur X: %f\n", futur.x);
+// 	printf("futur Y: %f\n\n", futur.y);
+
+// 	printf("actual coordonnee X: %d\n", pxl_to_coord(g->player.pos.x));
+// 	printf("actual coordonnee Y: %d\n", pxl_to_coord(g->player.pos.y));
+// 	printf("futur coordonnee X: %d\n", pxl_to_coord(futur.x));
+// 	printf("futur coordonnee Y: %d\n\n", pxl_to_coord(futur.y));
+
+// 	printf("futur X : %d\n",
+//	g->map_datas.map[pxl_to_coord(futur.y)][pxl_to_coord(futur.x)]);
+// 	if (g->map_datas.map[pxl_to_coord(futur.y)][pxl_to_coord(futur.x)] == '0')
+// 	{
+// 		g->player.pos.x += cos(g->player.initial_angle) * g->player.speed;
+// 		g->player.pos.y += sin(g->player.initial_angle) * g->player.speed;
+// 	}
+// }
